d19fe7005b796d99e9a267f383eca44a00ab563c
JuanManuelBR <2420211018@estudiantesunibague.edu.co>
2025-10-20 11:58:41 -0500

    Add server

---
+++ server/TiendaElectrodomesticos/src/test/java/cal/example/TiendaElectrodomesticos/TiendaElectrodomesticosApplicationTests.java
@@ -0,0 +1,13 @@
+package cal.example.TiendaElectrodomesticos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaElectrodomesticosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
